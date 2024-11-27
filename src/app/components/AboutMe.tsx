9c8a224fa56dbd7cc607e350ce3eec425a46052f
Jan Inge <jistrandman@gmail.com>
2024-11-27 14:01:00 +0100

    bugfix deployment issue

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function AboutMe() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from './components/Hero'
 import Projects from './components/Projects'
-import AboutMe from './components/AboutMe'
+
 
 export default function Home() {
   return (
