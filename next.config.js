22cf65e9ec6e76507779fac3119619d36f678415
Jan Inge <jistrandman@gmail.com>
2024-11-27 14:13:46 +0100

    more bugfix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'https://jan-inge-portfolio-lxice5dew-beachman90s-projects.vercel.app/'],
+  },
 }
 
 module.exports = nextConfig
